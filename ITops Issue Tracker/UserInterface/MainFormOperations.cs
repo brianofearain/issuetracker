ec2ada1aff625fd69460385dda521f3a8efa8c3b
brianofearain <brianjferan@gmail.com>
2015-02-17 14:21:14 -0800

    Update MainFormOperations.cs

--- ITops Issue Tracker/UserInterface/MainFormOperations.cs
+++ ITops Issue Tracker/UserInterface/MainFormOperations.cs
@@ -4,26 +4,10 @@ using System.Text;
 
 namespace IssueTracker
 {
-   /// <summary>
-   /// 
-   /// </summary>
    internal interface IMainFormOperations
    {
-      /// <summary>
-      /// 
-      /// </summary>
       void OnFormLoading();
-
-      /// <summary>
-      /// 
-      /// </summary>
-      /// 
-      /// <param name="answer"></param>
       void SetAnswer(string answer);
-
-      /// <summary>
-      /// 
-      /// </summary>
       void Cancel();
    }
 }
