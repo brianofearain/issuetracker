e2ddbc0844cf09dc92393e787f0a701192895cb3
brianofearain <brianjferan@gmail.com>
2015-02-17 14:18:34 -0800

    Update QuestionsSettings.cs

--- ITops Issue Tracker/Core/QuestionsSettings.cs
+++ ITops Issue Tracker/Core/QuestionsSettings.cs
@@ -4,30 +4,16 @@ using System.Text;
 
 namespace IssueTracker
 {
-   /// <summary>
-   /// 
-   /// </summary>
    internal enum FileTypeforOutput
    {
       TextType,
       Xml
    }
 
-   /// <summary>
-   /// 
-   /// </summary>
    internal static class QuestionSettings
    {
       public static readonly List<QuestionDeclarations> QuestionsInfo = new List<QuestionDeclarations>()
       {      
-
-          /* This is a sample:
-
-         new QuestionInfo(
-            "Do you want a tissue?", // The question
-            "Here you go."),                // The advice (null if none)
-
-          */
 
          new QuestionDeclarations(
             "Which Product is this issue occuring with?", "give the name"),
@@ -48,30 +34,10 @@ new QuestionDeclarations("please give a description of the environment you are w
       
       };
 
-      /// <summary>
-      /// The folder in which the output file will be stored.
-      /// </summary>
-      /// 
-      /// <remarks>
-      /// If this is not a null reference, it must be valid as a folder path;
-      /// the application will create the folder if it doesn't exist. If this
-      /// is a null reference, the folder in which the executable resides will
-      /// be the output folder.
-      /// </remarks>
       public static readonly string OutputFolderPath = null;
 
-      /// <summary>
-      /// The base name of the output file.
-      /// </summary>
-      /// 
-      /// <remarks>
-      /// The application assumes that this is valid as a base file name.
-      /// </remarks>
       public static readonly string OutputFileBaseName = "Answers";
 
-      /// <summary>
-      /// The type of the output file.
-      /// </summary>
       public static readonly FileTypeforOutput OutputFileType = FileTypeforOutput.TextType;
    }
 }
