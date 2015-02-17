7067ac5e34af57a178d3ab83b2813883c7eae3d1
brianofearain <brianjferan@gmail.com>
2015-02-17 14:17:48 -0800

    Update QuestionInfo.cs

--- ITops Issue Tracker/Core/QuestionInfo.cs
+++ ITops Issue Tracker/Core/QuestionInfo.cs
@@ -4,37 +4,18 @@ using System.Text;
 
 namespace IssueTracker
 {
-   /// <summary>
-   /// 
-   /// </summary>
     internal class QuestionDeclarations
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// 
-        /// <param name="question"></param>
-        /// <param name="advice">
-        /// Set to null if no advice is associated.
         public QuestionDeclarations(string question, string advice)
         {
             Question = question;
             Advice = advice;
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
         public string Question;
 
-        /// <summary>
-        /// Set to null if no advice is associated.
-        /// </summary>
         public string Advice;
 
-        /// <summary>
-        /// 
-        /// </summary>
         public string Answer;
     }
 }
